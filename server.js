35f496a0e75df97b61924e8f2092e3d0e0744e33
Aditi <aditig006.com>
2020-11-27 19:48:09 +0530

    changed port

--- server.js
+++ server.js
@@ -137,7 +137,8 @@ io.on('connection', socket => {
 })
 
 
-const PORT = 4000 || process.env.PORT;
+const PORT = process.env.PORT || 4000;
+
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
